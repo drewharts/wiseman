2d4409d535e216992dd023a405b96b68798ea851
drewhartsfield <drewharts8@gmail.com>
2023-05-25 17:12:58 -0600

    minor changes to login

--- login.js
+++ login.js
@@ -1,6 +1,5 @@
 function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
-    //can store it here and display further in future
-    // window.location.href = "play.html";
+    window.location.href = "spotify.html";
   }
