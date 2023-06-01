aeef384f87851a0b2a32b3b4df79b83cdd22556a
drewhartsfield <drewharts8@gmail.com>
2023-06-01 15:14:28 -0600

    updating deployment file

--- deployFiles.sh
+++ deployFiles.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 while getopts k:h:s: flag
 do
     case "${flag}" in
@@ -11,19 +9,40 @@ done
 
 if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
-    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
+    printf "  syntax: deployService.sh -k <pem key file> -h <hostname> -s <service>\n\n"
     exit 1
 fi
 
-printf "\n----> Deploying files for $service to $hostname with $key\n"
+printf "\n----> Deploying $service to $hostname with $key\n"
 
 # Step 1
-printf "\n----> Clear out the previous distribution on the target.\n"
-ssh -i "$key" ubuntu@$hostname << ENDSSH
-rm -rf services/${service}/public
-mkdir -p services/${service}/public
-ENDSSH
+printf "\n----> Build the distribution package\n"
+rm -rf dist
+mkdir dist
+cp -r public dist
+cp *.js dist
+cp *.json dist
 
 # Step 2
-printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i "$key" * ubuntu@$hostname:services/$service/public
+printf "\n----> Clearing out previous distribution on the target\n"
+ssh -i "$key" ubuntu@$hostname << ENDSSH
+rm -rf services/${service}
+mkdir -p services/${service}
+ENDSSH
+
+# Step 3
+printf "\n----> Copy the distribution package to the target\n"
+scp -r -i "$key" dist/* ubuntu@$hostname:services/$service
+
+# Step 4
+printf "\n----> Deploy the service on the target\n"
+ssh -i "$key" ubuntu@$hostname << ENDSSH
+bash -i
+cd services/${service}
+npm install
+pm2 restart ${service}
+ENDSSH
+
+# Step 5
+printf "\n----> Removing local copy of the distribution package\n"
+rm -rf dist
