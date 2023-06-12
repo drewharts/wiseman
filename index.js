c4be5a01d31b59c7ca2de782b63e20eb2cbb6789
drewhartsfield <drewharts8@gmail.com>
2023-06-12 15:49:46 -0600

    figuring out websocket

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 require('dotenv').config()
+const http = require('http');
 // const apiKey = process.env.OPENAI_API_KEY; //ChatGPT api key
 const jsonData = require('./data.json');
 const apiKey = jsonData.key; 
 const app = express();
+const WebSocket = require('ws');
 app.use(express.static('public'));
 app.use(express.json());
 const DB = require('./database.js');
@@ -14,25 +16,23 @@ const modelId = 'gpt-3.5-turbo';
 
 const port = 4000;
 
-// server.js
+//web socket code
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-const WebSocket = require('ws');
-
-const server = new WebSocket.Server({ port: 8080 });
-
-server.on('connection', ws => {
-  ws.on('message', message => {
-    console.log('Received: ', message);
-    server.clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+      // Broadcast the message to all clients
+      wss.clients.forEach((client) => {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+              client.send(message);
+          }
+      });
   });
-  
-  ws.on('close', () => {
-    console.log('Client disconnected');
-  });
+});
+
+server.listen(8080, () => {
+  console.log('Server is running on port 5000');
 });
 
 
