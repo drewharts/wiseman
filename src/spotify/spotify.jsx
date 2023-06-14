d993a019d73924f169068da1c1e835f5461c06fe
drewhartsfield <drewharts8@gmail.com>
2023-06-14 17:36:42 -0600

    fixing redirect links

--- src/spotify/spotify.jsx
+++ src/spotify/spotify.jsx
@@ -34,7 +34,7 @@ export function Spotify() {
         params.append("client_id", clientId);
         params.append("grant_type", "authorization_code");
         params.append("code", code);
-        params.append("redirect_uri", "https://startup.drewharts.com/spotify.html");
+        params.append("redirect_uri", "https://startup.drewharts.com/spotify");
         params.append("code_verifier", verifier);
     
         const result = await fetch("https://accounts.spotify.com/api/token", {
@@ -75,7 +75,7 @@ export function Spotify() {
         const params = new URLSearchParams();
         params.append("client_id", clientId);
         params.append("response_type", "code");
-        params.append("redirect_uri", "https://startup.drewharts.com/spotify.html");
+        params.append("redirect_uri", "https://startup.drewharts.com/spotify");
         params.append("scope", "user-read-private user-read-email");
         params.append("code_challenge_method", "S256");
         params.append("code_challenge", challenge);
