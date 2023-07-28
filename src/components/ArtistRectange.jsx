667ad577b13df6fac993f9d37db1fa8df29e45c7
drewhartsfield <drewharts8@gmail.com>
2023-07-28 17:41:13 -0600

    updating component

--- src/components/ArtistRectange.jsx
+++ src/components/ArtistRectange.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const ArtistRectangle = ({ title }) => {
+const ArtistRectangle = ({ title, children }) => {
   return (
     <div>
       <h1>{title}</h1>
+      <div>{children}</div>
       <img src="https://github.com/drewharts/startup/blob/main/OrangeArtistRectangle.png?raw=true" alt="Orange Rectangle" />
     </div>
   );
