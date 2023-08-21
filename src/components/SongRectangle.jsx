88683da6f6dfbd1151dbf98b0fd2edd593a6ce24
drewhartsfield <drewharts8@gmail.com>
2023-08-21 16:43:00 -0600

    proper commenting

--- src/components/SongRectangle.jsx
+++ src/components/SongRectangle.jsx
@@ -4,7 +4,7 @@ import './SongRectangle.css';
 const Songrectangle = ({ children }) => {
   return (
         <img class = "image-component" src="https://github.com/drewharts/startup/blob/main/songRectangle.png?raw=true" alt="Rectangle" />
-        #make text overlay here
+        //make text overlay here
   );
 };
 
